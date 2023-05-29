603c8912d830c770403fc22998ec5bf92da8d10a
DonDonut1 <Daniel-Weih@web.de>
2023-05-29 22:48:55 +0200

    added

---
+++ autoinstall.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Package installscript for Arch Linux
+
+echo "Downloading..."
+sleep 1
+
+sudo pacman -S 
+base-devel 
+git 
+polybar 
+rofi 
+picom 
+nitrogen 
+dunst 
+pavucontrol 
+lxappearance 
+lxsession 
+xdg-user-dirs 
+numlockx 
+ristretto 
+scrot 
+xorg-xset 
+xorg-xsetroot 
+zathura 
+zathura-pdf-mupdf 
+gedit 
+cmatrix 
+blender 
+gimp 
+steam 
+neofetch 
+libreoffice-fresh 
+libreoffice-fresh-de 
+firefox 
+thunderbird 
+thunar 
+thunar-archive-plugin 
+thunar-media-tags-plugin 
+thunar-volman 
+alacritty 
+ntfs-3g 
+gvfs 
+gzip 
+lsd 
+zsh 
+vim 
+xarchiver 
+htop 
+discord 
+gparted 
+gsimplecal 
+ufw 
+galculator
+ruby
