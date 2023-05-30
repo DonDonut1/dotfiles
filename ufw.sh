a7f6c62ea817e231141d935dec1e8cdcde2aa503
DonDonut1 <Daniel-Weih@web.de>
2023-05-30 17:20:49 +0200

    added

---
+++ ufw.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+sudo ufw limit 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw allow 1714:1764/udp
+sudo ufw allow 1714:1764/tcp
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
